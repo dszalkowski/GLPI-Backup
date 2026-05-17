96a4658c4e6afc5ab161e088ac718540cfb89833
Denis Szalkowski <38377408+dszalkowski@users.noreply.github.com>
2026-05-17 04:24:13 +0000

    Create full-timestamp-glpi-backup.sh

---
+++ full-timestamp-glpi-backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+DJ=$(date +%a|cut -d '.' -f 1) 
+tar cfz /home/temp/glpi.$DJ.tgz -C /home/www glpi 
+mariadb-dump --add-drop-database -B glpi -E -F --flush-privileges --single-transaction --protocol=socket -R | gzip > /home/temp/glpi.$DJ.sql.gz 
+mariadb-dump -B mysql -E -F --flush-privileges -n --replace -x --protocol=socket -R --skip-add-drop-table | gzip > /home/temp/mysql.$DJ.sql.gz
